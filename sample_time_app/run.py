5776b468944be1a570298d77c51e2d56dfbea2bd
sashadsouza02 <sasha.dsouza0202@gmail.com>
2024-02-18 21:57:43 -0500

    dcn lab 2 files

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from datetime import datetime
+import pytz
+
 app = Flask(__name__)
 
+@app.route('/time')
+def time():
+    # Set the timezone to Eastern Time
+    eastern = pytz.timezone('US/Eastern')
+    # Get the current time in UTC and convert it to Eastern Time
+    time_now = datetime.now(pytz.utc).astimezone(eastern)
+    # Format the time as a string
+    formatted_time = time_now.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+    return formatted_time
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
-
-app.run(host='0.0.0.0',
-        port=8080,
-        debug=True)
